2c83f852a517dd85f27e3f74ff46b1da3a379f49
Victor-BwD <victor.bogdanowdornelles@gmail.com>
2021-09-07 20:31:07 -0300

    Collisions and gravity added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { FreeCamera, Vector3, HemisphericLight, MeshBuilder } from '@babylonjs/c
 import SceneComponent from 'babylonjs-hook';
 import './App.css';
 
-let box;
+let box, ground;
 
-const GROUND_SIZE = 8;
+const GROUND_SIZE = 21;
 const BOXES_SIZE = 2;
 
 function Movement(camera){
@@ -15,18 +15,27 @@ function Movement(camera){
   camera.keysRight.push(68);
 }
 
+function MakeCameraCollision(camera, scene){
+  scene.gravity = new Vector3(0, -0.15, 0);
+  camera.applyGravity = true;
+  camera.ellipsoid = new Vector3(1, 1, 1);
+  scene.collisionsEnabled = true;
+  camera.checkCollisions = true;
+}
+
 const onSceneReady = scene => {
   // This creates and positions a free camera (non-mesh)
   var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
 
   // This targets the camera to scene origin
   camera.setTarget(Vector3.Zero());
-  
 
   const canvas = scene.getEngine().getRenderingCanvas();
 
   // This attaches the camera to the canvas
   camera.attachControl(canvas, true);
+  
+  MakeCameraCollision(camera, scene);
 
   Movement(camera);
 
@@ -41,9 +50,14 @@ const onSceneReady = scene => {
 
   // Move the box upward 1/2 its height
   box.position.y = 1;
+  box.checkCollisions = true; // Collision box
 
   // Our built-in 'ground' shape.
-  MeshBuilder.CreateGround("ground", {width: GROUND_SIZE, height: GROUND_SIZE}, scene);
+  ground = MeshBuilder.CreateGround("ground", {width: GROUND_SIZE, height: GROUND_SIZE}, scene);
+  ground.checkCollisions = true; // Collision Ground
+  
+
+  
 }
 
 /**
