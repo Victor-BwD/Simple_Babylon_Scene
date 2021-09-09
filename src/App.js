70a22e0f1edc264fcd99099ec49843ddcff35520
Victor-BwD <victor.bogdanowdornelles@gmail.com>
2021-09-09 15:41:30 -0300

    [UPDATED] Point n click added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FreeCamera, Vector3, HemisphericLight, MeshBuilder } from '@babylonjs/core';
+import { FreeCamera, Vector3, HemisphericLight, MeshBuilder, Quaternion, PointerEventTypes, Animation } from '@babylonjs/core';
 import SceneComponent from 'babylonjs-hook';
 import './App.css';
 
@@ -7,6 +7,8 @@ let box, ground;
 
 const GROUND_SIZE = 21;
 const BOXES_SIZE = 2;
+
+var useRotationNotTarget, useBox;
 
 function Movement(camera){
   camera.keysUp.push(87);
@@ -21,6 +23,44 @@ function MakeCameraCollision(camera, scene){
   camera.ellipsoid = new Vector3(1, 1, 1);
   scene.collisionsEnabled = true;
   camera.checkCollisions = true;
+}
+
+function PointnClick(scene, camera){
+  scene.onPointerObservable.add(function (pointerInfo) {
+    switch (pointerInfo.type) {
+        case PointerEventTypes.POINTERPICK:
+            let m = pointerInfo.pickInfo.pickedMesh;
+
+            if (m.name === "ground") {
+
+                var position = pointerInfo.pickInfo.pickedPoint
+                position.y += 1
+                var direction = pointerInfo.pickInfo.ray.direction
+                direction.y = 0
+                var target = position.add(direction)
+                var tempCamera = camera.clone()
+                tempCamera.position = position
+                tempCamera.setTarget(target)
+                var start = Quaternion.FromRotationMatrix(camera.getWorldMatrix())
+                var end = Quaternion.FromRotationMatrix(tempCamera.getWorldMatrix())
+                tempCamera.dispose()
+                //change options
+                useBox = false; //shows box transition instead of camera, but not with target animation
+                useRotationNotTarget = false;
+
+                var node = useBox ? box : camera;
+                Animation.CreateAndStartAnimation("transition", node, "position", 60, 60, camera.globalPosition, position, Animation.ANIMATIONLOOPMODE_CONSTANT);
+                if (useRotationNotTarget) {
+                  Animation.CreateAndStartAnimation("transition", node, "rotation", 30, 30, start.toEulerAngles(), end.toEulerAngles(), Animation.ANIMATIONLOOPMODE_CONSTANT);
+                } else {
+                  Animation.CreateAndStartAnimation("transition", node, "target", 6, 4, camera.getTarget(), target, Animation.ANIMATIONLOOPMODE_CONSTANT);
+                }
+
+            }
+
+            break;
+    }
+});
 }
 
 const onSceneReady = scene => {
@@ -38,6 +78,8 @@ const onSceneReady = scene => {
   MakeCameraCollision(camera, scene);
 
   Movement(camera);
+
+  PointnClick(scene, camera);
 
   // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
   var light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
@@ -77,7 +119,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <SceneComponent antialias onSceneReady={onSceneReady} onRender={onRender} id='my-canvas' />
+        <SceneComponent antialias onSceneReady={onSceneReady} onRender={onRender} id='my-canvas' style={{width:"100vw", height: "100vh"}} />
       </header>
     </div>
   );
